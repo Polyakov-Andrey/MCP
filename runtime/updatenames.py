8920f35705c7d62bcdad430b1f41e5f06626d881
Kenneth Endfinger <kaendfinger@gmail.com>
2012-10-29 19:43:28 -0400

    New branch to preserve MCP 7.2

--- runtime/updatenames.py
+++ runtime/updatenames.py
@@ -16,25 +16,15 @@ from mcp import updatenames_side
 
 def main():
     parser = OptionParser(version='MCP %s' % Commands.fullversion())
-    parser.add_option('--client', dest='only_client', action='store_true', help='only process client', default=False)
-    parser.add_option('--server', dest='only_server', action='store_true', help='only process server', default=False)
     parser.add_option('-f', '--force', action='store_true', dest='force', help='force update', default=False)
     parser.add_option('-c', '--config', dest='config', help='additional configuration file')
     options, _ = parser.parse_args()
-    updatenames(options.config, options.force, options.only_client, options.only_server)
+    updatenames(options.config, options.force)
 
 
-def updatenames(conffile, force, only_client, only_server):
+def updatenames(conffile, force):
     try:
         commands = Commands(conffile)
-
-        # client or server
-        process_client = True
-        process_server = True
-        if only_client and not only_server:
-            process_server = False
-        if only_server and not only_client:
-            process_client = False
 
         if not force:
             print 'WARNING:'
@@ -47,10 +37,8 @@ def updatenames(conffile, force, only_client, only_server):
                 print 'You have not entered "Yes", aborting the update process'
                 sys.exit(1)
 
-        if process_client:
-            updatenames_side(commands, CLIENT)
-        if process_server:
-            updatenames_side(commands, SERVER)
+        updatenames_side(commands, CLIENT)
+        updatenames_side(commands, SERVER)
     except Exception:  # pylint: disable-msg=W0703
         logging.exception('FATAL ERROR')
         sys.exit(1)
